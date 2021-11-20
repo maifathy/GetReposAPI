e9ab84d8be90f525be34cc001248948154fc6241
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-11-20 13:37:59 +0200

    update date string

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,6 @@
-const api = `https://api.github.com/search/repositories?q=created:>${new Date()}&sort=stars&order=desc`;
+const date = new Date().getFullYear()
+  + '-' + parseInt(new Date().getMonth() + 1)
+  + '-' + new Date().getDate() - 1;
+const api = `https://api.github.com/search/repositories?q=created:>${date}&sort=stars&order=desc`;
 
 export default api;
