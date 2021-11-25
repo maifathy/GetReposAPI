9a001e1fff7b38ff870eb9d52bb2a8dd777f8e63
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-11-25 18:36:23 +0200

    fixed eslint errors

--- api/controller.js
+++ api/controller.js
@@ -3,46 +3,52 @@ import repos from '../service/repos.js';
 const controller = {
   getLanguages: (req, res) => {
     repos.get()
-    .then((response) => {
-      let items = response.data.items;
-      let counter = 0;
-      let languages;
-      const allLangs = items.map((item) => item.language);
-      const obj = response.data.items.reduce((result, currentValue) => {
-        (
-          result[currentValue['language']]
-          = result[currentValue['language']] || []
-        )
-        .push(currentValue.name);
-        return result;
-      },
-      {});
-      languages =  [...new Set(allLangs.map((lang) => lang || "No Language"))];
-      const langs = Object.values(obj).reduce((result, repos) => {
-        const count = {
-          total: repos.length
+      .then((response) => {
+        let items = response.data.items;
+        let counter = 0;
+        let languages;
+        const allLangs = items.map((item) => item.language);
+        const obj = response.data.items.reduce(
+          (result, currentValue) => {
+            let newResult = result;
+            (
+              newResult[currentValue.language] = newResult[currentValue.language] || []
+            )
+              .push(currentValue.name);
+            return newResult;
+          },
+          {}
+        );
+        languages = [...new Set(allLangs.map((lang) => lang || 'No Language'))];
+        const langs = Object.values(obj).reduce(
+          (result, list) => {
+            let newResult = result;
+            let newList = list;
+            const count = {
+              total: list.length
+            };
+            newList = { list, count };
+            newResult[languages[counter]] = newList;
+            counter += 1;
+            return newResult;
+          },
+          {}
+        );
+        res.send(JSON.stringify(langs));
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log('Error', error.message);
         }
-        repos = {repos, count};
-        result[languages[counter]] = repos;
-        counter += 1;
-        return result;
-      },
-      {});
-      res.send(JSON.stringify(langs));
-    })
-    .catch((error) => {
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
-      res.send(JSON.stringify({Error: 'An error has been happened, please try again later'}));
-    })
+        console.log(error.config);
+        res.send(JSON.stringify({ Error: 'An error has been happened, please try again later' }));
+      });
   }
 };
 
