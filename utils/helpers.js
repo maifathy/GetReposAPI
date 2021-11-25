17bc2cf8fc493b32b526b61acc6285fcf1e80256
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-11-25 13:20:12 +0200

    fixed parsing of date

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 const date = new Date().getFullYear()
   + '-' + parseInt(new Date().getMonth() + 1)
-  + '-' + new Date().getDate() - 1;
+  + '-' + parseInt(new Date().getDate() - 1);
 const api = `https://api.github.com/search/repositories?q=created:>${date}&sort=stars&order=desc`;
 
 export default api;
